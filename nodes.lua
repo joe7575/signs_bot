6701f4f4af28ed418b15833dbd38bcad859e49d5
nixnoxus <nixnoxus@web.de>
2022-02-21 19:37:28 +0100

    use `farming.registered_plants`

--- nodes.lua
+++ nodes.lua
@@ -44,42 +44,14 @@ end
 -- Farming Redo
 -------------------------------------------------------------------------------
 if farming.mod == "redo" then
-	fp("farming:seed_wheat", "farming:wheat_1", "farming:wheat_8")
-	fp("farming:seed_cotton", "farming:cotton_1", "farming:cotton_8")
-	fp("farming:carrot", "farming:carrot_1", "farming:carrot_8")
-	fp("farming:potato", "farming:potato_1", "farming:potato_4")
-	fp("farming:tomato", "farming:tomato_1", "farming:tomato_8")
-	fp("farming:cucumber", "farming:cucumber_1", "farming:cucumber_4")
-	fp("farming:corn", "farming:corn_1", "farming:corn_8")
-	fp("farming:coffee_beans", "farming:coffee_1", "farming:coffee_5")
-	fp("farming:melon_slice", "farming:melon_1", "farming:melon_8")
-	fp("farming:pumpkin_slice", "farming:pumpkin_1", "farming:pumpkin_8")
-	fp("farming:raspberries", "farming:raspberry_1", "farming:raspberry_4")
-	fp("farming:blueberries", "farming:blueberry_1", "farming:blueberry_4")
-	fp("farming:rhubarb", "farming:rhubarb_1", "farming:rhubarb_3")
-	fp("farming:beans", "farming:beanpole_1", "farming:beanpole_5")
-	fp("farming:grapes", "farming:grapes_1", "farming:grapes_8")
-	fp("farming:seed_barley", "farming:barley_1", "farming:barley_7")
-	fp("farming:chili_pepper", "farming:chili_1", "farming:chili_8")
-	fp("farming:seed_hemp", "farming:hemp_1", "farming:hemp_8")
-	fp("farming:seed_oat", "farming:oat_1", "farming:oat_8")
-	fp("farming:seed_rye", "farming:rye_1", "farming:rye_8")
-	fp("farming:seed_rice", "farming:rice_1", "farming:rice_8")
-	fp("farming:beetroot", "farming:beetroot_1", "farming:beetroot_5")
-	fp("farming:cocoa_beans", "farming:cocoa_1", "farming:cocoa_4")
-	fp("farming:garlic_clove", "farming:garlic_1", "farming:garlic_5")
-	fp("farming:onion", "farming:onion_1", "farming:onion_5")
-	fp("farming:pea_pod", "farming:pea_1", "farming:pea_5")
-	fp("farming:peppercorn", "farming:pepper_1", "farming:pepper_5")
-	fp("farming:pineapple_top", "farming:pineapple_1", "farming:pineapple_8")
+	for _, def in pairs(farming.registered_plants) do
+		fp(def.seed, def.crop .. "_1", def.crop .. "_" .. def.steps)
+	end
 end
 
 -------------------------------------------------------------------------------
 -- Ethereal Farming
 -------------------------------------------------------------------------------
---fn("ethereal:strawberry_8",   "ethereal:strawberry 2",	     "ethereal:strawberry 1")
---fn("ethereal:onion_5",		  "ethereal:wild_onion_plant 2", "ethereal:onion_1")
-
 
 --fn("ethereal:willow_trunk",   "ethereal:willow_trunk", "ethereal:willow_sapling")
 --fn("ethereal:redwood_trunk",  "ethereal:redwood_trunk",  "ethereal:redwood_sapling")
